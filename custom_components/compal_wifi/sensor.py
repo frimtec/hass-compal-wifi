90613192cd8c4282db645716fdc5f2f462df1f17
Markus Friedli <frimtec@gmx.ch>
2021-07-03 07:59:42 +0200

    Process sensor values

--- custom_components/compal_wifi/sensor.py
+++ custom_components/compal_wifi/sensor.py
@@ -38,15 +38,31 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     compal_config = hass.data[DOMAIN]
     add_entities(
         [
-            ModemSensor("Compal Wifi Modem Model", "model", compal_config),
             ModemSensor(
-                "Compal Wifi Modem Hardware Version", "hw_version", compal_config
+                "Compal Wifi Modem Model",
+                compal_config,
+                lambda modem: modem["model"],
             ),
             ModemSensor(
-                "Compal Wifi Modem Software Version", "sw_version", compal_config
+                "Compal Wifi Modem Hardware Version",
+                compal_config,
+                lambda modem: modem["hw_version"],
             ),
-            ModemSensor("Compal Wifi Modem Operator", "operator_id", compal_config),
-            ModemSensor("Compal Wifi Modem Uptime", "uptime", compal_config),
+            ModemSensor(
+                "Compal Wifi Modem Software Version",
+                compal_config,
+                lambda modem: modem["sw_version"].replace(modem["model"] + "-", "", 1),
+            ),
+            ModemSensor(
+                "Compal Wifi Modem Operator",
+                compal_config,
+                lambda modem: modem["operator_id"],
+            ),
+            ModemSensor(
+                "Compal Wifi Modem Uptime",
+                compal_config,
+                lambda modem: modem["uptime"].split(":", 1)[0],
+            ),
         ]
     )
 
@@ -54,12 +70,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class ModemSensor(Entity):
     """Representation of a sensor."""
 
-    def __init__(self, name, attribute, compal_config):
+    def __init__(self, name, compal_config, attribute_accessor):
         """Initialize the sensor."""
         self._name = name
-        self._attribute = attribute
         self._compal_config = compal_config
-        self._state = compal_config.current_modem_state["modem"][attribute]
+        self._attribute_accessor = attribute_accessor
+        self._state = attribute_accessor(compal_config.current_modem_state["modem"])
 
     @property
     def name(self):
@@ -80,4 +96,6 @@ class ModemSensor(Entity):
         ):
             modem_status(self._compal_config)
 
-        self._state = self._compal_config.current_modem_state["modem"][self._attribute]
+        self._state = self._attribute_accessor(
+            self._compal_config.current_modem_state["modem"]
+        )
